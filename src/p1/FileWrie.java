179350951d5adc9fd7a4fed5540528fb3a35dcc0
Makakmayum Farid Khan <faridmakak754@gmail.com>
2025-11-01 10:52:17 +0530

    writer

--- src/p1/FileWrie.java
+++ src/p1/FileWrie.java
@@ -7,53 +7,19 @@ public class FileWrie {
 
 	public static void main(String[] args) {
 	
-		try {
-			//write
-			Scanner scan=new Scanner(System.in);
-			
-			System.out.println("Enter some text");
-			
-			
-			FileWriter fw=new FileWriter("fdkhanz.txt",true);
-			
-			while(true)
-			{
-				String data =scan.nextLine();
-				if(data.equalsIgnoreCase("exit"))
-				{
-					break;
-				}
 	
-				fw.write(data +"\n");
-				
-			}
-			fw.close();
-			scan.close();	
-			System.out.println("File create successfull");		
-		}
-		catch(Exception e)
-		{
-			System.out.println("File create failed");
-			
-		}
-		
 		try {
 			
-			BufferedReader br=new BufferedReader(new FileReader("fdkhanz.txt"));
-			String line;
-			System.out.println("Reading from file");
-			while((line=br.readLine()) !=null)
-					{
-						System.out.println(line);
-					}	
+			FileWriter fw=new FileWriter("text.txt");
+			fw.write("Hi i m testing file Writer now ");
+			fw.close();
+			System.out.println("Text save successfull !");
+					
 		}
-		
-		
 		catch(Exception e)
 		{
-			System.out.println("Read File failed");
+			System.out.println("File writer failed");
 		}
-
 		
 	}
 
